bac856dfe53c30c75ba230d4f8087558bd119d3e
Kornflex28 <chauvet.louis@gmail.com>
2018-12-01 17:11:08 +0100

    typo

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -23,15 +23,17 @@ class Requests(Resource):
 
 api.add_resource(Requests, '/requests')
 
+
 @app.route("/")
 @app.route("/home")
-def index():
+def home():
     return render_template("home.html", title="Home")
+
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html",title="About")
+    return render_template("about.html", title="About")
 
 
 if __name__ == '__main__':
