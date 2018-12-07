aab18b84f39ff44c2b065889937f71c66e59740e
Kornflex28 <chauvet.louis@gmail.com>
2018-12-07 15:17:51 +0100

    trainRecord restructure

--- database/TrainRecord.py
+++ database/TrainRecord.py
@@ -1,11 +1,13 @@
 from mongoengine import *
+from database.Station import Station
+from database.Proposition import Proposition
 
 
 class TrainRecord(Document):
-    origin = ReferenceField('Station', reverse_delete_rule=DO_NOTHING)
-    destination = ReferenceField('Station', reverse_delete_rule=DO_NOTHING)
+    origin = ReferenceField(Station, reverse_delete_rule=DO_NOTHING)
+    destination = ReferenceField(Station, reverse_delete_rule=DO_NOTHING)
     duration = IntField()
-    propositions = ListField(ReferenceField('Proposition',
+    propositions = ListField(ReferenceField(Proposition,
                                             reverse_delete_rule=DO_NOTHING))
     departureTime = DateTimeField()
     arrivalTime = DateTimeField()
