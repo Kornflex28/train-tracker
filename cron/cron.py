a9438f6b60e852dab5614dae13fe3428358c631a
TeddyTrqt <turquet.teddy@gmail.com>
2018-12-28 03:08:46 +0100

    update cron.py

--- cron/cron.py
+++ cron/cron.py
@@ -6,11 +6,20 @@ import datetime as dt
 from database.Request import Request as dbRequest
 from travel import Travel
 from utils.date import DateTime
+import datetime as dt
+
 
 import utils.credentials
-
-for request in dbRequest.objects():
+req = dbRequest.objects()
+for request in req:
     if request.uniqueDate:
         travels = Travel.get_all_travels(request.date, request.origin.code, request.destination.code)
         for travel in travels:
             travel.save_to_database()
+    else:
+        dates = [dt.datetime.now() + dt.timedelta(days=i)
+                 for i in range(request.gapTime)]
+        for date in dates:
+            travels = Travel.get_all_travels(date, request.origin.code, request.destination.code)
+            for travel in travels:
+                travel.save_to_database()
