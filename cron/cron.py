ef2925aaa44493f167dc4ec438c0e6063cd11d6d
Kornflex28 <chauvet.louis@gmail.com>
2019-02-13 13:56:46 +0100

    changed the gap time comportment

--- cron/cron.py
+++ cron/cron.py
@@ -1,5 +1,5 @@
 import datetime as dt
-import sys
+import sys,time
 
 #sys.path.append("/root/train-tracker/")
 sys.path.append("..")
@@ -12,17 +12,29 @@ from database.Request import Request as dbRequest
 
 
 req = dbRequest.objects()
-for request in req:
+n_req = len(req)
+for k,request in enumerate(req):
+    print("Request {}/{}".format(k+1,n_req))
     if request.uniqueDate:
         travels = Travel.get_all_travels(
             request.date, request.origin.code, request.destination.code)
         for travel in travels:
-            travel.save_to_database()
+            # travel.save_to_database()
+            a=1
     else:
-        dates = [dt.datetime.now() + dt.timedelta(days=i)
-                 for i in range(request.gapTime)]
-        for date in dates:
+        if dt.datetime.now() < request.date  :
+            dates = [request.date + dt.timedelta(days=i)
+                    for i in range(request.gapTime)]
+        else :
+            delta = request.date.now() - request.date
+            dates = [request.date.now() + dt.timedelta(days=delta.days)
+                    for i in range(request.gapTime)]
+        n_dates = len(dates)
+        sys.stdout.write('\r[{}] {}% '.format(' '*100, 0))
+        for i,date in enumerate(dates):
             travels = Travel.get_all_travels(
                 date, request.origin.code, request.destination.code)
             for travel in travels:
-                travel.save_to_database()
+                # travel.save_to_database()
+                a=1
+            sys.stdout.write('\r[{}{}] {}% '.format('#'*int(100*(i+1)/n_dates), ' '*int(100*(1-(i+1)/n_dates)), int(100*(i+1)/n_dates)))
